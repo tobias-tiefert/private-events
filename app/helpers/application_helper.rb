4a562114a31ad685ab746dca900763214d03148b
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-24 23:22:09 +0200

    Add css tokkens and style the navigation
    I added the first css files
    - reset
    - tokkens: colors, typography, spaces (empty yet), effects (empty yet)
    - navigation
    
    I styled the navigation.
    
    To be able to style the navigation item for the current page differently
    I created a small helper class: #navigation_item(name, path)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navigation_item(name, path)
+    if current_page?(path)
+      link_to name, path, class: "navigation-item navigation-item--active"
+    else
+      link_to name, path, class: "navigation-item"
+    end
+  end
 end
