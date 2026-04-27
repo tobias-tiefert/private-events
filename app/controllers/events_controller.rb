4b1e50d51ec9c475016a6149767d3627075e1d3b
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-27 10:56:35 +0200

    Add Rails' default authentication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[ index show ]
   def index
     @events = Event.all
   end
